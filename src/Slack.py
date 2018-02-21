5873da156076922c9cf9765744bf913a6f98daf4
Peter <dante383@protonmail.com>
2018-02-21 16:14:11 +0100

    follow guidelines

--- src/Slack.py
+++ src/Slack.py
@@ -25,7 +25,7 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
-# Check if Api Key are supplied. Else the bridge won"t be able to start at all.
+# Check if Api Key are supplied. Else the bridge won't be able to start at all.
 if SlackApiKey == "":
 	logger.error("There wasn't an API key for Slack specified. Quitting...")
 	sys.exit(1)
@@ -38,16 +38,16 @@ if DiscordApiKey == "":
 sc = SlackClient(SlackApiKey)
 sc.rtm_connect()
 
-# Set up URL"s and headers for Discord.
+# Set up URL's and headers for Discord.
 headers = { "Authorization":"Bot {}".format(DiscordApiKey),
 			"User-Agent":"DiscordSlackBridge (http://reddit.com/r/teendeveloper, v0.1)",
 "Content-Type":"application/json", }
 
 # Run the Bridge 
 
-# we have to query all users in slack to map their id to their names later. querying every message
+# We have to query all users in slack to map their id to their names later. Querying every message
 # does not have sense, so we will just do it now and then flush the cache when someone will
-# enter the slack. easy enough
+# enter the slack. Easy enough
 
 try:
 	usersList = sc.api_call(
@@ -55,7 +55,7 @@ try:
 		channel = "#general"
 	)['members'] 
 except:
-	logger.error("[Slack] Failed to fetch users list! Converting mentions won\"t be available.")
+	logger.error('[Slack] Failed to fetch users list! Converting mentions won\'t be available.')
 
 while True:
 	try:
@@ -78,16 +78,16 @@ while True:
 			else:
 				pass	
 
-			# message formatting section
+			# Message formatting section
 
-			# mentions. valid mention in slack looks like this: <@username>
+			# Mentions. Valid mention in slack looks like this: <@username>
 			if "@" in message: 
-				# split message to a spaces and then detect which of the expressions starts with <@ and ends with >
+				# Split message to a spaces and then detect which of the expressions starts with <@ and ends with >
 				expressions = message.split(' ')
 				for expression in expressions:
 					if expression[:2] == '<@' and expression[-1:] == '>':
 						userId = expression[2:-1]
-						# now lets map it to username (if usersList exists)
+						# Now lets map it to username (if usersList exists)
 						if (usersList):
 							for user in usersList: 
 								if (user['id'] == userId):
